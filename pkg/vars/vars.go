920c4790a4100ff583fa13a8cc71b8a4b5d4e93c
cuisongliu <cuisongliu@qq.com>
2021-04-07 13:09:36 +0800

    修复多个版本同时的问题

--- pkg/vars/vars.go
+++ pkg/vars/vars.go
@@ -11,27 +11,12 @@ var (
 	MarketCtlToken string
 	IsArm64        bool
 
-	SSHCmdDownload    = "https://github.com/cuisongliu/sshcmd/releases/download/v%s/sshcmd%s"                 //sshcmd-arm64
-	SealosDownload    = "https://sealyun.oss-accelerate.aliyuncs.com/v%s/sealos%s"                            //sealos-arm64
-	MarketCtlDownload = "https://sealyun-market.oss-accelerate.aliyuncs.com/marketctl/v%s/linux_%s/marketctl" //linux_arm64
-
-	DockerShell = "wget https://download.docker.com/linux/static/stable/%s/docker-%s.tgz  " + //aarch64
-		"-O docker.tgz && cp docker.tgz kube/docker/docker.tgz"
-
-	ContainerdShell = "wget https://sealyun.oss-accelerate.aliyuncs.com/tools/cri-containerd-cni-%s-linux-%s.tar.gz " +
-		"-O cri-containerd-cni-linux.tar.gz && " +
-		"cp cri-containerd-cni-linux.tar.gz kube/containerd/cri-containerd-cni-linux.tar.gz"
-
-	CrictlShell = "wget https://sealyun.oss-accelerate.aliyuncs.com/tools/crictl-v%s-linux-%s.tar.gz " +
-		"-O  crictl.tar.gz &&  tar xf crictl.tar.gz && cp crictl kube/bin/"
-
-	KubeShell = "wget https://dl.k8s.io/v%s/kubernetes-server-linux-%s.tar.gz -O  kubernetes-server.tar.gz && " + //arm64
-		"tar zxvf kubernetes-server.tar.gz"
-	KubeVersion      string
-	DefaultPrice     float64
-	DefaultZeroPrice float64
-	DefaultClass     = "cloud_kernel" //cloud_kernel
-	DefaultProduct   = "kubernetes"   //kubernetes
+	SSHCmdDownload, SealosDownload, MarketCtlDownload, DockerShell, ContainerdShell, CrictlShell, KubeShell string
+	KubeVersion                                                                                             string
+	DefaultPrice                                                                                            float64
+	DefaultZeroPrice                                                                                        float64
+	DefaultClass                                                                                            = "cloud_kernel" //cloud_kernel
+	DefaultProduct                                                                                          = "kubernetes"   //kubernetes
 )
 
 const (
@@ -40,7 +25,18 @@ const (
 	defaultCriCtlVersion     = "1.20.0"
 	defaultDockerVersion     = "19.03.12"
 	defaultContainerdVersion = "1.4.3"
-
+	FmtSSHCmdDownload        = "https://github.com/cuisongliu/sshcmd/releases/download/v%s/sshcmd%s"                 //sshcmd-arm64
+	FmtSealosDownload        = "https://sealyun.oss-accelerate.aliyuncs.com/v%s/sealos%s"                            //sealos-arm64
+	FmtMarketCtlDownload     = "https://sealyun-market.oss-accelerate.aliyuncs.com/marketctl/v%s/linux_%s/marketctl" //linux_arm64
+	FmtDockerShell           = "wget https://download.docker.com/linux/static/stable/%s/docker-%s.tgz  " +           //aarch64
+		"-O docker.tgz && cp docker.tgz kube/docker/docker.tgz"
+	FmtContainerdShell = "wget https://sealyun.oss-accelerate.aliyuncs.com/tools/cri-containerd-cni-%s-linux-%s.tar.gz " +
+		"-O cri-containerd-cni-linux.tar.gz && " +
+		"cp cri-containerd-cni-linux.tar.gz kube/containerd/cri-containerd-cni-linux.tar.gz"
+	FmtCrictlShell = "wget https://sealyun.oss-accelerate.aliyuncs.com/tools/crictl-v%s-linux-%s.tar.gz " +
+		"-O  crictl.tar.gz &&  tar xf crictl.tar.gz && cp crictl kube/bin/"
+	FmtKubeShell = "wget https://dl.k8s.io/v%s/kubernetes-server-linux-%s.tar.gz -O  kubernetes-server.tar.gz && " + //arm64
+		"tar zxvf kubernetes-server.tar.gz"
 	defaultSealosVersion    = "3.3.9-rc.1"
 	defaultMarketCtlVersion = "1.0.5" //v1.0.5
 	defaultSSHCmdVersion    = "1.5.5"
@@ -80,17 +76,17 @@ func platform() map[string]map[bool]string {
 }
 
 func LoadVars() error {
-	KubeShell = fmt.Sprintf(KubeShell, KubeVersion, platform()["kube"][IsArm64])
+	KubeShell = fmt.Sprintf(FmtKubeShell, KubeVersion, platform()["kube"][IsArm64])
 	//sshcmd
-	SSHCmdDownload = fmt.Sprintf(SSHCmdDownload, defaultSSHCmdVersion, platform()["sshcmd"][IsArm64])
+	SSHCmdDownload = fmt.Sprintf(FmtSSHCmdDownload, defaultSSHCmdVersion, platform()["sshcmd"][IsArm64])
 	//sealos
-	SealosDownload = fmt.Sprintf(SealosDownload, defaultSealosVersion, platform()["sealos"][IsArm64])
+	SealosDownload = fmt.Sprintf(FmtSealosDownload, defaultSealosVersion, platform()["sealos"][IsArm64])
 	//marketctl
-	MarketCtlDownload = fmt.Sprintf(MarketCtlDownload, defaultMarketCtlVersion, platform()["marketctl"][IsArm64])
+	MarketCtlDownload = fmt.Sprintf(FmtMarketCtlDownload, defaultMarketCtlVersion, platform()["marketctl"][IsArm64])
 	//containerd
-	ContainerdShell = fmt.Sprintf(ContainerdShell, defaultContainerdVersion, platform()["containerd"][IsArm64])
-	DockerShell = fmt.Sprintf(DockerShell, platform()["docker"][IsArm64], defaultDockerVersion)
-	CrictlShell = fmt.Sprintf(CrictlShell, defaultCriCtlVersion, platform()["crictl"][IsArm64])
+	ContainerdShell = fmt.Sprintf(FmtContainerdShell, defaultContainerdVersion, platform()["containerd"][IsArm64])
+	DockerShell = fmt.Sprintf(FmtDockerShell, platform()["docker"][IsArm64], defaultDockerVersion)
+	CrictlShell = fmt.Sprintf(FmtCrictlShell, defaultCriCtlVersion, platform()["crictl"][IsArm64])
 	if IsArm64 {
 		DefaultProduct = "kubernetes-arm64"
 	}
