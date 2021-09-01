acf8d2abd80deead4c33dbd5e63a0a2b427e5cd0
cuisongliu <cuisongliu@qq.com>
2021-09-01 22:16:54 +0800

    fix rootfs

--- runtime/containerd/shell/init.sh
+++ runtime/containerd/shell/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install containerd
 chmod a+x containerd.sh
-bash containerd.sh
+source containerd.sh
 
 
 # 修改kubelet
@@ -11,9 +11,5 @@ cat > /etc/systemd/system/kubelet.service.d/containerd.conf << eof
 Environment="KUBELET_EXTRA_ARGS=--container-runtime=remote --cgroup-driver=${criDriver} --runtime-request-timeout=15m --container-runtime-endpoint=unix:///run/containerd/containerd.sock --image-service-endpoint=unix:///run/containerd/containerd.sock"
 eof
 
-cat /etc/systemd/system/kubelet.service.d/containerd.conf
-
 chmod a+x init-kube.sh
 bash init-kube.sh
-
-cat /var/lib/kubelet/config.yaml
