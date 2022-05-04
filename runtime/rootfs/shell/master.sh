87bcee5c4c61c1c9af0469629c975b24de013cc4
cuisongliu <cuisongliu@qq.com>
2022-05-04 16:26:45 +0800

    ecs

--- runtime/rootfs/shell/master.sh
+++ runtime/rootfs/shell/master.sh
@@ -2,6 +2,7 @@ kubeadm init --config ../conf/kubeadm.yaml
 mkdir ~/.kube
 cp /etc/kubernetes/admin.conf ~/.kube/config
 kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 kubectl apply -f ../conf/calico.yaml
 sleep 5
 kubectl apply -f ../conf/calico.yaml
